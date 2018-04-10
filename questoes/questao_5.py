11cd2c4994124d731dd84b5b9b4d3a292260829c
Peedrohj <38144264+Peedrohj@users.noreply.github.com>
2018-04-10 10:19:13 -0300

    Update questao_5.py

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -11,9 +11,16 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 5")
-
-
+    lado1 = float(input('Me informe o primeiro lado: '))
+    lado2 = float(input('Me informe o segundo lado: '))
+    lado3 = float(input('Me informe o terceiro lado: '))
+    
+    if lado1 == lado2 == lado3:
+        print('O triangulo eh equilatero')
+    elif lado1 == lado2 != lado3 or lado2 == lado3 != lado1 or lado3 == lado1 != lado2:
+        print('O tirangulo eh isosceles')
+    else:
+        print('O triangulo eh escaleno')
     
 if __name__ == '__main__':
     main()
