f99214f9fbd4fd7e0201a79edffaa54d5ad70fdf
Peedrohj <38144264+Peedrohj@users.noreply.github.com>
2018-04-10 10:20:04 -0300

    Update questao_7.py

--- questoes/questao_7.py
+++ questoes/questao_7.py
@@ -19,9 +19,21 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 7")
+    ano = int(input('Me informe o ano: '))
+    bisexto = ano % 4
+    bisexto2 = ano % 400
+    if bisexto == 0:
+        if ano % 100 != 0:
+            print('O ano eh bisexto')
+        else:
+            print('O ano nao eh bisexto')
+    elif bisexto2 == 0:
+        if ano % 100 != 0:
+            print('O ano eh bisexto')
+        else:
+            print('O ano nao eh bisexto')
+    else:
+        print('O ano nao eh bisexto')
 
-
-    
 if __name__ == '__main__':
     main()
