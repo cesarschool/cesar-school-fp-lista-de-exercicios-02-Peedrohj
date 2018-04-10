3d8ffda6ca6d8d712857e01675ad1658698f2ea0
Peedrohj <38144264+Peedrohj@users.noreply.github.com>
2018-04-10 10:19:42 -0300

    Update questao_6.py

--- questoes/questao_6.py
+++ questoes/questao_6.py
@@ -12,9 +12,24 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 6")
-
-
+    cadeia = input('Me informe a cadeia de DNA: ').upper()
+    total = len(cadeia)
+    a = (100 * cadeia.count('A')) / total
+    t = (100 * cadeia.count('T')) / total
+    c = (100 * cadeia.count('C')) / total
+    g = (100 * cadeia.count('G')) / total
+    invalido = 100 - (a + c + t + g)
+    print('''A cadeia de DNA eh {:.2f}
+    A quantidade de adenina eh {:.2f}
+    A quantidade de tinina eh {:.2f}
+    A quantidade de citosina eh {:.2f}
+    A quantidade de guanina eh {:.2f}
+    A porcentagem de nucleotideos invalidos eh {:.2f}%'''.format(total, cadeia.count('A'), cadeia.count('T'), cadeia.count('C'), cadeia.count('G'), invalido))
+    
+    print('''A porcentagem de adenina equivale a {:.2f}%
+    A porcentagem de tinina equivale a {:.2f}%
+    A porcentagem de citosina equivale a {:.2f}%
+    A porcentagem de guanina equivale a {:.2f}%'''.format(a, t, c, g,))
     
 if __name__ == '__main__':
     main()
